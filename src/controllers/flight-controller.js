9e7b5d8dd45522f13ce2615f837dc719ef372f65
Ganivada Mouli <vishnumouli0@gmail.com>
2023-10-17 00:11:08 +0530

    Added error codes

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,4 +1,5 @@
 const { FlightService } = require("../services/index");
+const { ServerErrorCodes, SuccessCodes } = require("../utils/error-codes");
 
 const flightService = new FlightService();
 
@@ -14,7 +15,7 @@ const create = async (req, res) => {
       price: req.body.price,
     };
     const flight = await flightService.createFlight(flightRequestdata);
-    return res.status(201).json({
+    return res.status(SuccessCodes.CREATED).json({
       data: flight,
       success: true,
       message: "Successfully created a Flight",
@@ -22,7 +23,7 @@ const create = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({
+    return res.status(ServerErrorCodes.INTERNAL_SERVER_ERROR).json({
       data: {},
       success: false,
       message: "Can't Create Flight",
@@ -34,7 +35,7 @@ const create = async (req, res) => {
 const getAll = async (req, res) => {
   try {
     const flights = await flightService.getAllFlights(req.query);
-    return res.status(201).json({
+    return res.status(SuccessCodes.OK).json({
       data: flights,
       success: true,
       message: "Successfully Fetched Flights",
@@ -42,7 +43,7 @@ const getAll = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({
+    return res.status(ServerErrorCodes.INTERNAL_SERVER_ERROR).json({
       data: {},
       success: false,
       message: "Can't Fetch Flights",
